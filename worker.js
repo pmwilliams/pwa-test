ec45c785efc35ac9ef57ab2ba7504f2a603fd622
pmwilliams <pmwilliams@clara.net>
2022-02-21 22:18:08 +0000

    fixing styling and scrolling bugs

--- worker.js
+++ worker.js
@@ -1,7 +1,7 @@
 /* global clients */
 /* eslint-disable no-restricted-globals */
 
-const cacheName = 'pwa-test-v13';
+const cacheName = 'pwa-test-v14';
 const filesToCache = [
   'index.html',
   'style.css',
