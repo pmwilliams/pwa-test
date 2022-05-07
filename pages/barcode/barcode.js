14209317c127a8b04bb16853ffa982d9267c5a25
pmwilliams <pmwilliams@clara.net>
2022-05-07 21:49:50 +0100

    adding bounding box for barcode

--- pages/barcode/barcode.js
+++ pages/barcode/barcode.js
@@ -6,11 +6,25 @@ const app = document.querySelector('application-shell');
 
 const onScanClick = () => {
   const video = document.querySelector('.video');
+  const canvas = document.querySelector('.overlay');
+  const ctx = canvas.getContext('2d');
   const barcodeDetector = new window.BarcodeDetector({ formats: ['ean_13'] });
+
+  const draw = (boundingBox) => {
+    ctx.rect(boundingBox.x, boundingBox.y, boundingBox.width, boundingBox.y);
+    ctx.lineWidth = '1';
+    ctx.strokeStyle = 'red';
+    ctx.stroke();
+    window.requestAnimationFrame(draw);
+  };
 
   const detect = () => {
     barcodeDetector.detect(video).then((barcodes) => {
-      barcodes.forEach((barcode) => app.alert(`Barcode found: ${barcode.rawValue}`));
+      ctx.clearRect(0, 0, canvas.clientWidth, canvas.canvasHeight);
+      barcodes.forEach((barcode) => {
+        app.alert(`Barcode found: ${barcode.rawValue}`);
+        draw(barcode.boundingBox);
+      });
       window.requestAnimationFrame(detect);
     });
   };
@@ -32,6 +46,7 @@ const onScanClick = () => {
             video.play();
             window.requestAnimationFrame(() => {
               detect();
+              draw();
             });
           };
         });
--- worker.js
+++ worker.js
@@ -1,7 +1,7 @@
 /* global clients */
 /* eslint-disable no-restricted-globals */
 
-const cacheName = 'pwa-test-v19';
+const cacheName = 'pwa-test-v20';
 const filesToCache = [
   'index.html',
   'style.css',
